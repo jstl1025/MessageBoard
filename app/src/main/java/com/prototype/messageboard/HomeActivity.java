b9eb59e33a84f3e09572ca19c541f3293b7fa9f2
juiwen@ualberta.ca <jtuoa1234>
2018-07-25 12:04:37 -0600

    Vivian: Add toolbar, complete display setting activity, prelim design on Home Activity

--- app/src/main/java/com/prototype/messageboard/HomeActivity.java
+++ app/src/main/java/com/prototype/messageboard/HomeActivity.java
@@ -1,12 +1,16 @@
 package com.prototype.messageboard;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDelegate;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class HomeActivity extends NavigationDrawer{
+    ImageButton imageButton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +31,44 @@ public class HomeActivity extends NavigationDrawer{
         View contentView = inflater.inflate(R.layout.activity_home, null, false);
         mDrawerLayout.addView(contentView, 0);
 
+        addListenerOnButton();
+
+    }
+
+
+    public void addListenerOnButton() {
+        imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
+
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this,
+                        "ImageButton is clicked!", Toast.LENGTH_SHORT).show();
+
+                //Blur image
+//                Bitmap resultBmp = BlurBuilder.blur(HomeActivity.this, BitmapFactory.decodeResource(getResources(), R.drawable.baseline_android_black_18dp));
+//                ImageButton imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
+//                imageButton1.setImageBitmap(resultBmp);
+
+                startActivity(new Intent(HomeActivity.this, Pop.class));
+            }
+
+        });
+
+
+
+
+
+
     }
 
 }
+
+
+
+
+
+
 
 
 
--- app/src/main/java/com/prototype/messageboard/NavigationDrawer.java
+++ app/src/main/java/com/prototype/messageboard/NavigationDrawer.java
@@ -8,18 +8,26 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
 
 public class NavigationDrawer extends AppCompatActivity {
 
     protected DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     private NavigationView nv;
+    private Toolbar mToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_drawer);
+
+        //overlay toolbar
+        mToolbar = (Toolbar) findViewById(R.id.nav_action);
+        setSupportActionBar(mToolbar);
 
         //Add Drawer left swipe
         mDrawerLayout = (DrawerLayout) findViewById(R.id.homeLayout);
