735592d8411258a7aa51f1e6c8186bc2deedfb45
juiwen@ualberta.ca <jtuoa1234>
2018-07-28 10:23:34 -0600

    Vivian: Home activity modify send button

--- app/src/main/java/com/prototype/messageboard/HomeActivity.java
+++ app/src/main/java/com/prototype/messageboard/HomeActivity.java
@@ -7,10 +7,10 @@ import android.support.v7.app.AppCompatDelegate;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
-public class HomeActivity extends NavigationDrawer{
-    ImageButton imageButton1;
+public class HomeActivity extends NavigationDrawer implements View.OnClickListener{
+    ImageButton imageButton1, imageButton2, imageButton3, imageButton4, imageButton5, imageButton6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +22,6 @@ public class HomeActivity extends NavigationDrawer{
         }
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_home);
-
         // https://stackoverflow.com/questions/21405958/how-to-display-navigation-drawer-in-all-activities
         LayoutInflater inflater = (LayoutInflater) this
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -31,36 +30,87 @@ public class HomeActivity extends NavigationDrawer{
         View contentView = inflater.inflate(R.layout.activity_home, null, false);
         mDrawerLayout.addView(contentView, 0);
 
-        addListenerOnButton();
-
-    }
-
-
-    public void addListenerOnButton() {
-        imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
-
-        imageButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(HomeActivity.this,
-                        "ImageButton is clicked!", Toast.LENGTH_SHORT).show();
-
-                //Blur image
-//                Bitmap resultBmp = BlurBuilder.blur(HomeActivity.this, BitmapFactory.decodeResource(getResources(), R.drawable.baseline_android_black_18dp));
-//                ImageButton imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
-//                imageButton1.setImageBitmap(resultBmp);
-
-                startActivity(new Intent(HomeActivity.this, Pop.class));
-            }
-
-        });
-
-
-
-
+        //addListenerOnButton();
+        ImageButton one = (ImageButton) findViewById(R.id.imageButton1);
+        one.setOnClickListener(HomeActivity.this); //call on click event
+        ImageButton two = (ImageButton) findViewById(R.id.imageButton2);
+        two.setOnClickListener(HomeActivity.this);
+        ImageButton three = (ImageButton) findViewById(R.id.imageButton3);
+        three.setOnClickListener(HomeActivity.this);
+        ImageButton four = (ImageButton) findViewById(R.id.imageButton4);
+        four.setOnClickListener(HomeActivity.this);
+        ImageButton five = (ImageButton) findViewById(R.id.imageButton5);
+        five.setOnClickListener(HomeActivity.this);
+        ImageButton six = (ImageButton) findViewById(R.id.imageButton6);
+        six.setOnClickListener(HomeActivity.this);
 
 
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(HomeActivity.this, Pop.class);
+        switch(v.getId()) {
+            case R.id.imageButton1:
+//              Blur image
+//              Bitmap resultBmp = BlurBuilder.blur(HomeActivity.this, BitmapFactory.decodeResource(getResources(), R.drawable.baseline_android_black_18dp));
+//              ImageButton imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
+//              imageButton1.setImageBitmap(resultBmp);
+                intent.putExtra("imageButton", 1);
+                startActivity(intent);
+                break;
+            case R.id.imageButton2:
+                intent.putExtra("imageButton", 2);
+                startActivity(intent);
+                break;
+            case R.id.imageButton3:
+                intent.putExtra("imageButton", 3);
+                startActivity(intent);
+                break;
+            case R.id.imageButton4:
+                intent.putExtra("imageButton", 4);
+                startActivity(intent);
+                break;
+            case R.id.imageButton5:
+                intent.putExtra("imageButton", 5);
+                startActivity(intent);
+                break;
+            case R.id.imageButton6:
+                intent.putExtra("imageButton", 6);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+
+
+//    public void addListenerOnButton() {
+//        imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
+//        imageButton2 = (ImageButton) findViewById(R.id.imageButton2);
+
+//        imageButton1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(HomeActivity.this,
+//                        "ImageButton is clicked!", Toast.LENGTH_SHORT).show();
+//                //Blur image
+////                Bitmap resultBmp = BlurBuilder.blur(HomeActivity.this, BitmapFactory.decodeResource(getResources(), R.drawable.baseline_android_black_18dp));
+////                ImageButton imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
+////                imageButton1.setImageBitmap(resultBmp);
+//
+//                startActivity(new Intent(HomeActivity.this, Pop.class));
+//            }
+//        });
+
+
+
+
+
+
+
 
 }
 
--- app/src/main/java/com/prototype/messageboard/Pop.java
+++ app/src/main/java/com/prototype/messageboard/Pop.java
@@ -9,10 +9,9 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class Pop extends Activity{
-    Button sendButton1;
+public class Pop extends Activity {
+    Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -29,30 +28,78 @@ public class Pop extends Activity{
         getWindow().setGravity(Gravity.TOP);
         Window window = getWindow();
         WindowManager.LayoutParams params = window.getAttributes();
-        params.x = -280;
-        params.y = 360;
-        window.setAttributes(params);
-
         //dimension
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
         int width = dm.widthPixels;
-        width = width /5;
         int height = dm.heightPixels;
-        height = height/9;
-        window.setLayout(width,height);
+
+        Integer imageButtonVal;
+        Bundle extras = getIntent().getExtras();
+        imageButtonVal = extras.getInt("imageButton");
+
+
+        if (imageButtonVal == 1){
+            params.x = -280;
+            params.y = 360;
+            window.setAttributes(params);
+            width = width /5;
+            height = height/9;
+            window.setLayout(width,height);
+        }
+        else if (imageButtonVal == 2){
+            params.x = 280;
+            params.y = 360;
+            window.setAttributes(params);
+            width = width /5;
+            height = height/9;
+            window.setLayout(width,height);
+        }
+        else if (imageButtonVal == 3){
+            params.x = -280;
+            params.y = 755;
+            window.setAttributes(params);
+            width = width /5;
+            height = height/9;
+            window.setLayout(width,height);
+        }
+        else if (imageButtonVal == 4){
+            params.x = 280;
+            params.y = 755;
+            window.setAttributes(params);
+            width = width /5;
+            height = height/9;
+            window.setLayout(width,height);
+        }
+        else if (imageButtonVal == 5){
+            params.x = -280;
+            params.y = 1150;
+            window.setAttributes(params);
+            width = width /5;
+            height = height/9;
+            window.setLayout(width,height);
+        }
+        else if (imageButtonVal == 6){
+            params.x = 280;
+            params.y = 1150;
+            window.setAttributes(params);
+            width = width /5;
+            height = height/9;
+            window.setLayout(width,height);
+        }
+
 
         addListenerOnButton();
 
     }
 
     public void addListenerOnButton(){
-        sendButton1 = (Button) findViewById(R.id.send_button1);
-        sendButton1.setOnClickListener(new View.OnClickListener() {
+        sendButton = (Button) findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Toast.makeText(Pop.this,
-                    "ImageButton1 send!", Toast.LENGTH_SHORT).show();
+            // remove activity after click
+            finish();
         }
 
     });
